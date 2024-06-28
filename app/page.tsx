853915df19d931ce1a8fb68047b200d1debb5e7a
karthiksneu <sunil.k@northeastern.edu>
2024-06-28 12:32:44 -0400

    hero page and grid boxes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from 'react-icons/fa'
 import Image from "next/image";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Hello, Portfolio!</h1>
+    <main className="relative bg-black-100 flex justify-center items-center 
+    flex-col overflow-auto mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          { name: 'Home', link: '/', icon: <FaHome /> }
+        ]}
+        />
+        <Hero />
+        <Grid />
       </div>
     </main>
   );
