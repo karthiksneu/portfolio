e96d38f5b5814c18470d8d22a2eefaeadcd95b63
karthiksneu <sunil.k@northeastern.edu>
2024-07-02 14:20:03 -0400

    added bento box grid for the work page along with the index.ts file that stores all the data to be displayed on the page

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -7,12 +7,16 @@ const Grid = () => {
         <section id='about'>
             <BentoGrid>
                 {gridItems.map
-                    ((item, i) => (
+                    (({ id, title, description, className, img, imgClassName, titleClassName, spareImg }) => (
                         <BentoGridItem
-                            id={item.id}
-                            key={item.id}
-                            title={item.title}
-                            description={item.description}
+                            id={id}
+                            key={id}
+                            title={title}
+                            description={description}
+                            className={className}
+                            img={img}
+                            imgClassName={imgClassName}
+                            spareImg={spareImg}
                         />
                     ))
                 }
